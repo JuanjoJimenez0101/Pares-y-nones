dc8c8fe317e5927b77049e26412fc90263c74b7f
JuanjoJimenez0101 <juanjosejimenez1990@icloud.com>
2017-03-09 10:54:16 +0100

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,12 @@
+***juego de pares y nones
+Escenario: Pantalla de juego y pares o nones
+En principio el juego le hacemos entradas y salidas por consola.
+Dos jugadores
+
+El jugador 1 es nones 
+El jugador 2 es pares 
+Se le pide al jugador 1 que elija del 1 al 5
+Se le pide al jugador 2 que elija del 1 al 5
+Se suman y se hace modulo e la suma
+Si sale impares gana el jugador 1
+Si sale pares gana el jugador 2
---
+++ script.js
@@ -0,0 +1,16 @@
+document.write('vamos a jugar a pares y nones');
+var TiradaJugador1=prompt('jugador1 elije un numero del 1 al 5');
+document.write('el jugador 1 ha sacado' + jugador1);
+var TiradaJugador2=prompt('jugador2 elije un nimero del 1 al 5');
+document.write('el jugador 2 ha sacado'+ jugador2);
+TiradaJugador1=Number(TiradaJugador1);
+TiradaJugador2=Number(TiradaJugador2);
+var suma=TiradaJugador1 + TiradaJugador2;
+document.write('la suma de jugadas es =' + suma)
+
+if(suma % 2 === 0){ 
+    document.write('ha ganado el jugador1');
+}
+else{
+    document.write('ha ganado el jugador 2');
+}
