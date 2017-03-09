6f269afc6a64873d0903eb798692c7e4b3ff6878
JuanjoJimenez0101 <juanjosejimenez1990@icloud.com>
2017-03-09 11:05:20 +0100

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.write('vamos a jugar a pares y nones');
 var TiradaJugador1=prompt('jugador1 elije un numero del 1 al 5');
 document.write('el jugador 1 ha sacado' + jugador1);
-var TiradaJugador2=prompt('jugador2 elije un nimero del 1 al 5');
+var TiradaJugador2=prompt('jugador2 elije un numero del 1 al 5');
 document.write('el jugador 2 ha sacado'+ jugador2);
 TiradaJugador1=Number(TiradaJugador1);
 TiradaJugador2=Number(TiradaJugador2);
